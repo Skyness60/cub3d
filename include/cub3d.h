4cc6419b04cb4ea32e2447b39cf6ffc182fa92a2
Jean Lebard <jlebard@paul-f4Br4s3.clusters.42paris.fr>
2024-11-18 11:05:19 +0100

    debut

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,6 +1,13 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 # include "../export/ultimatelib.h"
+# include <math.h>
+
+typedef struct	s_look
+{
+	double	dirX;
+	double	dirY;
+}	t_look;
 
 typedef enum e_orientation
 {
---
+++ src/raycasting/raycasting.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlebard <jlebard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 09:30:48 by jlebard           #+#    #+#             */
+/*   Updated: 2024/11/18 10:16:50 by jlebard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+void	ray_loop(t_look *look, int	cell_size)
+{
+	double	tan;
+	int		i;
+
+	i = 0;
+	while(1)
+	{
+		tan = atan2(look->dirX, look->dirY);
+		if (is_wall() == 1)
+		{
+			display_plane(i);
+				break ;
+		}
+		else
+			display_f_and_c(i);
+		i += cell_size;
+	}
+}
