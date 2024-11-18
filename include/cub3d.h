ab0c6dd04aaaa0660280650bd38bfcb8c922e393
Sami Perron <sperron@paul-f4Br4s1.clusters.42paris.fr>
2024-11-18 09:44:09 +0100

    struct create in header

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,4 +1,37 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 # include "../export/ultimatelib.h"
+
+typedef enum e_orientation
+{
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST,
+}			t_orientation;
+
+typedef struct	s_texture
+{
+	char	*path;
+	void	*img;
+	int		*data;
+	int		width;
+	int		height;
+}				t_texture;
+
+typedef struct	s_map
+{
+	char	**map;
+	int		width;
+	int		height;
+}				t_map;
+
+typedef struct	s_data
+{
+	t_map			map;
+	t_texture		*texture;
+	unsigned long	hex_floor;
+	unsigned long	hex_ceiling;
+
+}				t_data;
 #endif // !CUB3D_H
--- src/cub3d.c
+++ src/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: sperron <sperron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 08:35:41 by sperron           #+#    #+#             */
-/*   Updated: 2024/11/18 08:41:22 by sperron          ###   ########.fr       */
+/*   Updated: 2024/11/18 09:43:14 by sperron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ int	main(int ac, char **av)
 {
 	if (ac != 2)
 		return (ft_dprintf(2, "Error\nWrong number of arguments\n"), 1);
-	(void)av;
+	
 }
